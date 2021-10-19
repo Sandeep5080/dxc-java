dc0752e4fc4caa1eea98b499463b60d106977093
sande <sande@LAPTOP-AL3NTDCH>
2021-10-19 21:15:17 +0530

    DebugDemo

---
+++ dxc-java/src/basic/Debug.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Debug {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int sum = calc.add(10, 20);
+		System.out.println("the sum is--"+sum);
+
+		throw new NullPointerException();
+	}
+
+}
+
+
