f74afb935fdd704eff727bad7013db24103b4519
sande <sande@LAPTOP-AL3NTDCH>
2021-10-20 11:42:52 +0530

    DefaultConstructor

---
+++ dxc-java/src/constructor/Student.java
@@ -0,0 +1,16 @@
+package constructor;
+
+public class Student {
+	int a;
+	   String b;
+	   public static void main(String[] args) {
+
+	      
+	      Student S = new Student();
+	      
+	      System.out.println(S.a);
+	      System.out.println(S.b);
+	   }
+	
+
+}
